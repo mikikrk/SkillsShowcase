41d43ab9529337c7b8a1b6b43af2590977c6dbfa
mikikrk <miki.krk@gmail.com>
2019-01-14 12:16:27 +0100

    Created and used base Dagger component
    Dagger isn't really necessary for such small application, but it helps to build nice architecture

---
+++ app/src/main/java/com/mikolajnowak/skillsshowcase/di/ApplicationComponent.kt
@@ -0,0 +1,30 @@
+package com.mikolajnowak.skillsshowcase.di
+
+import android.app.Application
+import com.mikolajnowak.skillsshowcase.SkillsShowcaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import dagger.android.support.DaggerApplication
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface ApplicationComponent : AndroidInjector<DaggerApplication> {
+
+    fun inject(application: SkillsShowcaseApplication)
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+}
