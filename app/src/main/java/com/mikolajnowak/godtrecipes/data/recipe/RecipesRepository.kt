43450d9b5eff7983d49cf549c0decd348b54051b
mikikrk <miki.krk@gmail.com>
2019-01-14 18:46:27 +0100

    Switched RecipesRepository to use Room instead of dummy

--- app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/RecipesRepository.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/RecipesRepository.kt
@@ -2,6 +2,7 @@ package com.mikolajnowak.godtrecipes.data.recipe
 
 import androidx.lifecycle.LiveData
 import com.mikolajnowak.godtrecipes.data.recipe.model.Recipe
+import com.mikolajnowak.godtrecipes.data.room.AppDatabase
 import com.mikolajnowak.godtrecipes.liveData.InitializedLiveData
 import dagger.Reusable
 import javax.inject.Inject
@@ -9,8 +10,13 @@ import javax.inject.Inject
 @Reusable
 class RecipesRepository @Inject constructor() {
 
-    fun getRecipes(searchQuery: String): LiveData<List<Recipe>> {
-        val recipes = listOf(Recipe("1", "title1", "https://i.pinimg.com/736x/37/2e/07/372e079d1ad511dbca4fd67e2feb51c1--strawberry-balsamic-icecream.jpg", "some description", listOf("sugar", "salt")))
-        return InitializedLiveData(recipes)
-    }
+    @Inject
+    lateinit var recipeDao: RecipeDao
+
+    fun getRecipes(searchQuery: String): LiveData<List<Recipe>> =
+            if (searchQuery.isEmpty()) {
+                recipeDao.getAllRecipies()
+            } else {
+                recipeDao.findRecipies(searchQuery)
+            }
 }
