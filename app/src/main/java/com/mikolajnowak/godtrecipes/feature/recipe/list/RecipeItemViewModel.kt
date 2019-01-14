0807d6a012c33ceb0ba3430dff54dc1134554670
mikikrk <miki.krk@gmail.com>
2019-01-14 16:23:37 +0100

    Created RecipeItemViewModel for wrapping Recipe for View
    Such wrapping helps keep items have similar logic approach as other parts of view. Also in case of any actions on the item, it's testable with unit tests on the item VM

---
+++ app/src/main/java/com/mikolajnowak/godtrecipes/feature/recipe/list/RecipeItemViewModel.kt
@@ -0,0 +1,36 @@
+package com.mikolajnowak.godtrecipes.feature.recipe.list
+
+import androidx.lifecycle.LiveData
+import com.mikolajnowak.godtrecipes.data.recipe.model.Recipe
+import com.mikolajnowak.godtrecipes.liveData.InitializedLiveData
+
+interface RecipeItemViewModel {
+
+    val title: LiveData<String>
+    val imageUrl: LiveData<String>
+    val description: LiveData<String>
+    val ingredients: LiveData<List<String>>
+}
+
+class RecipeItemViewModelImpl(
+
+    private val recipe: Recipe
+
+) : RecipeItemViewModel {
+
+    override val title = InitializedLiveData(recipe.title)
+    override val imageUrl = InitializedLiveData(recipe.imageUrl)
+    override val description = InitializedLiveData(recipe.description)
+    override val ingredients = InitializedLiveData(recipe.ingredients)
+
+    override fun equals(other: Any?): Boolean =
+        if (other is RecipeItemViewModelImpl) {
+            this.recipe == other
+        } else {
+            super.equals(other)
+        }
+
+    override fun hashCode(): Int {
+        return recipe.hashCode()
+    }
+}
