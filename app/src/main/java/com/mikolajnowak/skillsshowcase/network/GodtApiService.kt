ae21c56c1e1ff87e6c36b6c55e543b2b7b6efbf5
mikikrk <miki.krk@gmail.com>
2019-01-14 13:08:45 +0100

    Added GodtApiService
    `Limit` and `from` could be put in query path as constant but they are
    fields which look like they should be configured with code. Others seem
    constant for the app

---
+++ app/src/main/java/com/mikolajnowak/skillsshowcase/network/GodtApiService.kt
@@ -0,0 +1,12 @@
+package com.mikolajnowak.skillsshowcase.network
+
+import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GodtApiService {
+
+    @GET("getRecipesListDetailed?tags=&size=thumbnail-medium&ratio=1")
+    fun getRecipesListDetailed(@Query("limit") limit: Int = 50, @Query("from") from: Int = 0): Single<ResponseBody>
+}
