6a6181db4d3760d4a0cce06022a94b659b99e8ad
mikikrk <miki.krk@gmail.com>
2019-01-14 16:45:52 +0100

    Added retrieving ViewModel into Activity

--- app/src/main/java/com/mikolajnowak/godtrecipes/feature/recipe/RecipesActivity.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/feature/recipe/RecipesActivity.kt
@@ -1,10 +1,20 @@
 package com.mikolajnowak.godtrecipes.feature.recipe
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.mikolajnowak.godtrecipes.R
+import com.mikolajnowak.godtrecipes.di.viewModel.ViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class RecipesActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel: RecipesViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory).get(RecipesViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
