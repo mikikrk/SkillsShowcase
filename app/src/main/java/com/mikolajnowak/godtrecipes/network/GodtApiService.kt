9d61104c34b86d45c050726de4e08379cd264947
mikikrk <miki.krk@gmail.com>
2019-01-14 19:22:00 +0100

    Updated ApiService to return proper data

--- app/src/main/java/com/mikolajnowak/godtrecipes/network/GodtApiService.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/network/GodtApiService.kt
@@ -1,5 +1,6 @@
 package com.mikolajnowak.godtrecipes.network
 
+import com.mikolajnowak.godtrecipes.data.recipe.model.response.RecipeResponse
 import io.reactivex.Single
 import okhttp3.ResponseBody
 import retrofit2.http.GET
@@ -8,5 +9,5 @@ import retrofit2.http.Query
 interface GodtApiService {
 
     @GET("getRecipesListDetailed?tags=&size=thumbnail-medium&ratio=1")
-    fun getRecipesListDetailed(@Query("limit") limit: Int = 50, @Query("from") from: Int = 0): Single<ResponseBody>
+    fun getRecipesListDetailed(@Query("limit") limit: Int = 50, @Query("from") from: Int = 0): Single<List<RecipeResponse>>
 }
