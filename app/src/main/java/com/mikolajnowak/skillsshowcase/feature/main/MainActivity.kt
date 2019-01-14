79457fcf2e59bc1c2b07d1487c0a78c61a158baa
mikikrk <miki.krk@gmail.com>
2019-01-14 12:27:42 +0100

    Switched MainActivity to use Dagger
    It will be used in future to inject ViewModels factory

--- app/src/main/java/com/mikolajnowak/skillsshowcase/feature/main/MainActivity.kt
+++ app/src/main/java/com/mikolajnowak/skillsshowcase/feature/main/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.mikolajnowak.skillsshowcase.feature.main
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.mikolajnowak.skillsshowcase.R
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
