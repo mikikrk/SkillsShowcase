06e7e665ead18e67e51f63f5198558d18a7b09fe
mikikrk <miki.krk@gmail.com>
2019-01-14 12:30:06 +0100

    Created generic ViewModelFactory
    It works nicer when using such generic factory and just providers for all ViewModels instead of creating separate Factory for every ViewModel

---
+++ app/src/main/java/com/mikolajnowak/skillsshowcase/di/viewModel/ViewModelFactory.kt
@@ -0,0 +1,36 @@
+package com.mikolajnowak.skillsshowcase.di.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.multibindings.IntoMap
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelFactory @Inject constructor(
+
+        private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = findCreatorClass(modelClass)
+                ?: throw IllegalArgumentException("Unknown model class $modelClass. Check if you add @Provide for $modelClass and annotate it with @${IntoMap::class} and @${ViewModelKey::class}")
+        return creator.get() as T
+    }
+
+    private fun <T : ViewModel> findCreatorClass(modelClass: Class<T>): @JvmSuppressWildcards Provider<ViewModel>? {
+        var creator = creators[modelClass]
+        if (creator == null) {
+            for (entry in creators.entries) {
+                if (modelClass.isAssignableFrom(entry.key)) {
+                    creator = entry.value
+                    break
+                }
+            }
+        }
+        return creator
+    }
+}
