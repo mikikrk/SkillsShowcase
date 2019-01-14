c4c30cbe8ec763e5b1f935eafbce3e56376aa00e
mikikrk <miki.krk@gmail.com>
2019-01-14 16:56:22 +0100

    Added helper for listening input text changes
    Default implementation is unnecessarily complex for simple cases

--- app/src/main/java/com/mikolajnowak/godtrecipes/utils/AndroidExt.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/utils/AndroidExt.kt
@@ -1,9 +1,12 @@
 package com.mikolajnowak.godtrecipes.utils
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.annotation.LayoutRes
 
 val Context.inflater: LayoutInflater
@@ -11,3 +14,17 @@ val Context.inflater: LayoutInflater
 
 fun ViewGroup.inflate(@LayoutRes resId: Int, attachToRoot: Boolean = false): View =
     context.inflater.inflate(resId, this, attachToRoot)
+
+fun EditText.addOnTextChangedListener(callback: (CharSequence?, Int, Int, Int) -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            callback(s, start, before, count)
+        }
+    })
+}
