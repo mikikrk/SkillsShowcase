453467cc6e75093935d6698c22d1a2d7ac0e1809
mikikrk <miki.krk@gmail.com>
2019-01-14 19:01:49 +0100

    Created Api Response models

---
+++ app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/model/response/ImageResponse.kt
@@ -0,0 +1,5 @@
+package com.mikolajnowak.godtrecipes.data.recipe.model.response
+
+data class ImageResponse(
+        val url: String
+)
---
+++ app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/model/response/IngredientResponse.kt
@@ -0,0 +1,5 @@
+package com.mikolajnowak.godtrecipes.data.recipe.model.response
+
+data class IngredientResponse(
+        val name: String
+)
