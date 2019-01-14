a6d369f32d0f25e58b756b63decc255c8b78031e
mikikrk <miki.krk@gmail.com>
2019-01-14 13:20:03 +0100

    Added providing GodtApiService to NetworkModule

--- app/src/main/java/com/mikolajnowak/skillsshowcase/network/NetworkModule.kt
+++ app/src/main/java/com/mikolajnowak/skillsshowcase/network/NetworkModule.kt
@@ -1,10 +1,14 @@
 package com.mikolajnowak.skillsshowcase.network
 
+import com.google.gson.Gson
 import com.mikolajnowak.skillsshowcase.BuildConfig
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -36,5 +40,17 @@ class NetworkModule {
     @Singleton
     internal fun getHttpClient(builder: OkHttpClient.Builder): OkHttpClient {
         return builder.build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideApiService(httpClient: OkHttpClient, gson: Gson): GodtApiService {
+        return Retrofit.Builder()
+            .client(httpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(BuildConfig.API_URL)
+            .build()
+            .create(GodtApiService::class.java)
     }
 }
