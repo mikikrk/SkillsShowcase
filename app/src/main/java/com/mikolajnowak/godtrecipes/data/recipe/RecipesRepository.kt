367e913429b20c27369c064b3ab7768174edeb1b
mikikrk <miki.krk@gmail.com>
2019-01-14 19:23:31 +0100

    Updated repository to load data from server to database

--- app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/RecipesRepository.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/data/recipe/RecipesRepository.kt
@@ -2,9 +2,12 @@ package com.mikolajnowak.godtrecipes.data.recipe
 
 import androidx.lifecycle.LiveData
 import com.mikolajnowak.godtrecipes.data.recipe.model.Recipe
-import com.mikolajnowak.godtrecipes.data.room.AppDatabase
-import com.mikolajnowak.godtrecipes.liveData.InitializedLiveData
+import com.mikolajnowak.godtrecipes.data.recipe.model.toEntity
+import com.mikolajnowak.godtrecipes.liveData.addSimpleSource
+import com.mikolajnowak.godtrecipes.liveData.rx.DisposableMediatorLiveData
+import com.mikolajnowak.godtrecipes.network.GodtApiService
 import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @Reusable
@@ -13,10 +16,35 @@ class RecipesRepository @Inject constructor() {
     @Inject
     lateinit var recipeDao: RecipeDao
 
+    @Inject
+    lateinit var godtApi: GodtApiService
+
     fun getRecipes(searchQuery: String): LiveData<List<Recipe>> =
-            if (searchQuery.isEmpty()) {
-                recipeDao.getAllRecipies()
-            } else {
-                recipeDao.findRecipies(searchQuery)
+            DisposableMediatorLiveData<List<Recipe>>().apply {
+                addDisposable(loadFromApiIntoDB())
+                addSimpleSource(getRoomList(searchQuery))
             }
+
+    private fun loadFromApiIntoDB() =
+            godtApi.getRecipesListDetailed()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.computation())
+                    .map { recipesList ->
+                        recipesList.map { recipeResponse ->
+                            recipeResponse.toEntity()
+                        }
+                    }
+                    .subscribe({ recipesList ->
+                        recipeDao.insertAll(recipesList)
+                    }, {
+                        //TODO return to UI
+                    })
+
+    private fun getRoomList(searchQuery: String): LiveData<List<Recipe>> {
+        return if (searchQuery.isEmpty()) {
+            recipeDao.getAllRecipies()
+        } else {
+            recipeDao.findRecipies(searchQuery)
+        }
+    }
 }
