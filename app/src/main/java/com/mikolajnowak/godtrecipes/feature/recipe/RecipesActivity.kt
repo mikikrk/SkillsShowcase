0dffd2432911454aeff7da000ed9c1d21157a203
mikikrk <miki.krk@gmail.com>
2019-01-14 16:49:41 +0100

    Added setting up RecyclerView

--- app/src/main/java/com/mikolajnowak/godtrecipes/feature/recipe/RecipesActivity.kt
+++ app/src/main/java/com/mikolajnowak/godtrecipes/feature/recipe/RecipesActivity.kt
@@ -2,9 +2,13 @@ package com.mikolajnowak.godtrecipes.feature.recipe
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mikolajnowak.godtrecipes.R
 import com.mikolajnowak.godtrecipes.di.viewModel.ViewModelFactory
+import com.mikolajnowak.godtrecipes.feature.recipe.list.RecipesAdapter
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.recipes_activity.*
 import javax.inject.Inject
 
 class RecipesActivity : DaggerAppCompatActivity() {
@@ -15,9 +19,16 @@ class RecipesActivity : DaggerAppCompatActivity() {
     private val viewModel: RecipesViewModel by lazy {
         ViewModelProviders.of(this, viewModelFactory).get(RecipesViewModel::class.java)
     }
+    private val adapter = RecipesAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.recipes_activity)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        recipesRV.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recipesRV.adapter = adapter
     }
 }
